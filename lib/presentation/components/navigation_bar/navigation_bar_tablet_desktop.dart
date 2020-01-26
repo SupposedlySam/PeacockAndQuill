db929208c58d8cd5be166e8660abfa55ef03befc
SupposedlySam <supposedlysam@gmail.com>
2020-01-25 18:05:27 -0700

    Remove login with google button from main screen

--- lib/presentation/components/navigation_bar/navigation_bar_tablet_desktop.dart
+++ lib/presentation/components/navigation_bar/navigation_bar_tablet_desktop.dart
@@ -3,25 +3,10 @@ import 'navigation_bar_imports.dart';
 class NavigationBarTabletDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<IAuthorizationUseCase>(context);
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
         NavBarLogo(),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            RaisedButton(
-              child: Text('Login with Google'),
-              onPressed: auth.googleSignIn,
-            ),
-            SizedBox(
-              width: 60,
-            ),
-            // NavBarItem('About', AboutRoute),
-          ],
-        )
       ],
     );
   }
